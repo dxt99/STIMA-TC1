2849cddc93ff74092bb70e5bc17767540915b665
dxt99 <ericlouis19x@gmail.com>
2022-01-19 15:00:16 +0700

    added constants

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 8 search directions
+int dx[8]={0,0,1,1,1,-1,-1,-1};
+int dy[8]={1,-1,0,1,-1,0,1,-1};
+
+// Puzzle
+vector<vector<char>> arr;
+
+// Words
+vector<string> s;
+
+// Answers
+struct info{
+	int x;
+	int y;
+	int dir; //direction
+	int l; //length
+};
+
+vector<info> ans;
+
+// Main
+
+int main(){
+	// input
+	
+	
+}
